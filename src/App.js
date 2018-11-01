b61c01ea2f66861ae82a6aa41dd97a14232da45d
jiasheng_ou <oujia6014@foxmail.com>
2018-11-01 17:27:20 +0800

    第二阶段

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import PropTypes from 'prop-types'
 // 用户输入框
 class CommentInput extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func
+  }
   constructor () {
     super()
     this.state = {
@@ -23,12 +26,32 @@ class CommentInput extends Component {
     console.log('评论内容:'+event.target.value)
   }
   handleSubmit (){
-    if(this.props.onSubmit){
-      const { username, content } = this.state
-      this.props.onSubmit({username,content})
+    if (this.props.onSubmit) {
+      this.props.onSubmit({
+        username: this.state.username,
+        content: this.state.content,
+        createdTime: +new Date()
+      })
     }
     this.setState({content:''})
     console.log('点击发布:')
+  }
+  componentWillMount () {
+    this._loadUsername()
+  }
+  _loadUsername () {
+    const username = localStorage.getItem('username')
+    console.log('获取用户名:'+username)
+    if (username) {
+      this.setState({ username })
+    }
+  }
+  _saveUsername (username) {
+    localStorage.setItem('username', username)
+  }
+
+  handleUsernameBlur (event) {
+    this._saveUsername(event.target.value)
   }
   render (){
     return (
@@ -36,7 +59,7 @@ class CommentInput extends Component {
       <div className='comment-field'>
         <span className='comment-field-name'>用户名：</span>
         <div className='comment-field-input'>
-          <input value={this.state.username}  onChange={this.handleUsernameChange.bind(this)}/>
+          <input value={this.state.username}  onChange={this.handleUsernameChange.bind(this)} onBlur={this.handleUsernameBlur.bind(this)} />
         </div>
       </div>
       <div className='comment-field'>
@@ -56,28 +79,93 @@ class CommentInput extends Component {
 }
 
 
-
 class CommentList extends Component {
+  static propTypes = {
+    comments: PropTypes.array,
+    onDeleteComment: PropTypes.func
+  }
   static defaultProps = {
     comments: []
+  }
+  handleDeleteComment (index) {
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(index)
+    }
   }
   render(){
     return (
       <div>
-        {this.props.comments.map((comment,i) => <Comment comment={comment} key={i} />)}
+        {this.props.comments.map((comment,i) => 
+          <Comment comment={comment} key={i} index={i} onDeleteComment={this.handleDeleteComment.bind(this)} />
+        )}
       </div>
     )
   }
 }
 
+
+
+
 class Comment extends Component {
+  static propTypes = {
+    comment: PropTypes.object.isRequired,
+    onDeleteComment: PropTypes.func,
+    index: PropTypes.number
+  }
+  constructor () {
+    super()
+    this.state = { timeString: '' }
+  }
+  componentWillMount () {
+    this._updateTimeString()
+    this._timer = setInterval(
+      this._updateTimeString.bind(this),
+      5000
+    )
+  }
+  componentWillUnmount () {
+    clearInterval(this._timer)
+  }
+
+  _updateTimeString () {
+    const comment = this.props.comment
+    const duration = (+Date.now() - comment.createdTime) / 1000
+    this.setState({
+      timeString: duration > 60
+        ? `${Math.round(duration / 60)} 分钟前`
+        : `${Math.round(Math.max(duration, 1))} 秒前`
+    })
+  }
+  handleDeleteComment () {
+    if (this.props.onDeleteComment) {
+      this.props.onDeleteComment(this.props.index)
+    }
+  }
+  _getProcessedContent(content){
+    return content
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;")
+      .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+  }
   render () {
+    const { comment } = this.props
     return (
       <div className='comment'>
         <div className='comment-user'>
-          <span>{this.props.comment.username} </span>：
+          <span>{comment.username} </span>：
         </div>
-        <p>{this.props.comment.content}</p>
+        <p dangerouslySetInnerHTML={{
+            __html: this._getProcessedContent(comment.content)
+          }} />
+        <span className='comment-createdtime'>
+          {this.state.timeString}
+        </span>
+        <span className='comment-delete' onClick={this.handleDeleteComment.bind(this)}>
+          删除
+        </span>
       </div>
     )
   }
@@ -90,22 +178,41 @@ class App extends Component {
       comments: []
     }
   }
-
+  componentWillMount () {
+    this._loadComments()
+  }
+  _loadComments () {
+    let comments = localStorage.getItem('comments')
+    console.log('获取评论列表:'+comments)
+    if (comments) {
+      comments = JSON.parse(comments)
+      this.setState({ comments })
+    }
+  }
+  handleDeleteComment (index) {
+    const comments = this.state.comments
+    comments.splice(index, 1)
+    this.setState({ comments })
+    this._saveComments(comments)
+  }
   handleSubmitComment (comment){
     if (!comment) return
     if (!comment.username) return alert('请输入用户名')
     if (!comment.content) return alert('请输入评论内容')
-    this.state.comments.push(comment)
-    this.setState({
-      comments: this.state.comments
-    })
     console.log('提交参数:'+JSON.stringify(comment))
+    const comments = this.state.comments
+    comments.push(comment)
+    this.setState({ comments })
+    this._saveComments(comments)
+  }
+  _saveComments (comments) {
+    localStorage.setItem('comments', JSON.stringify(comments))
   }
   render(){
     return (
       <div className="wrapper">
         <CommentInput onSubmit={this.handleSubmitComment.bind(this)} /> 
-        <CommentList comments={this.state.comments} />
+        <CommentList comments={this.state.comments} onDeleteComment={this.handleDeleteComment.bind(this)} />
       </div>
     )
   }
